af4cbd7f168f96581f6181cff364f912d7f0d028
mohasali <msalim1244@gmail.com>
2025-03-29 18:03:30 +0000

    Update sketch_mar29a.ino
    SALIM AND TOBI COOKED!!!;
    completed remap function

--- Arduino Code/sketch_mar29a/sketch_mar29a.ino
+++ Arduino Code/sketch_mar29a/sketch_mar29a.ino
@@ -17,32 +17,47 @@ void setup() {
 }
 
 void loop(){
-	strip.setPixelColor(remap(1,5),100,0,0);
-	strip.setPixelColor(remap(2,5),100,0,0);
-	strip.setPixelColor(remap(3,5),100,0,0);
-	strip.setPixelColor(remap(4,5),100,0,0);
-	strip.setPixelColor(remap(5,5),100,0,0);
-
-  strip.show();
+  for(int i = 1; i < 21; ++i)
+  {
+    for(int j = 1; j < 11; ++j)
+    {
+     strip.setPixelColor(remap(j, i), 45, 20, 79);
+    }
+    strip.show();
+    delay(200);
+    strip.clear();
+  }
   
-  delay(200);
 }
 
 // check disatance from end of row
 // use distance as a measure how down to go
 
 uint16_t remap(uint16_t x, uint16_t y) {
-    if (x < 6) {
-      if(x%2){
+    if (x <= 6) 
+    {
+      if(x%2)
+      {
         //Odd rows, They go up in counting
         return (x-1)*40 + (y*2)-1;
       }
-      else{
-        //Posotive rows, They go down in counting
+      else
+      {
+        //Positive rows, They go down in counting
         return x*40 - (y*2);
-
       }
-    } else {
+    } 
+    else 
+    {
+      if(x%2)
+      {
+        return 240 + (x-7)*20 + y-1;
+      }
+      else {
+      {
+        return 240 + (x-6)*20 - (y);
+      }
+      }
     }
 }
 
